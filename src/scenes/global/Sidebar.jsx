3da4a7c3baee4f7498e95a277097d2a08ba0b3d7
simo-sw <simonecapelli13@gmail.com>
2023-10-31 08:57:14 +0100

    Tables - Team section

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -130,6 +130,13 @@ const Sidebar = () => {
               Data
             </Typography>
             <Item
+              title={'Manage Team'}
+              to="/team"
+              icon={<PeopleOutlinedIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
               title={'Contacts Information'}
               to="/contacts"
               icon={<ContactsOutlinedIcon />}
